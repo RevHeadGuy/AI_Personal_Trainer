cfe267872c6bee4277fe11109d6b89754bb927ac
Vedant Atri <126312692+RevHeadGuy@users.noreply.github.com>
2025-04-16 01:47:45 +0530

    Add files via upload

---
+++ data.py
@@ -0,0 +1,12 @@
+exercise_db = {
+    "full_body": [
+        {"name": "Jumping Jacks", "equipment": "none"},
+        {"name": "Bodyweight Squats", "equipment": "none"},
+        {"name": "Push-ups", "equipment": "none"},
+        {"name": "Plank", "equipment": "none"}
+    ],
+    "knee_safe": [
+        {"name": "Glute Bridge", "equipment": "none"},
+        {"name": "Lying Leg Raises", "equipment": "none"}
+    ]
+}
